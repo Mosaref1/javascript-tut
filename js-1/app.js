850c5419293c03aedb587d6dbac62906484997d7
Mosaref1 <mosarefpaik763@gmail.com>
2023-11-22 15:49:38 +0530

    javascript loop

---
+++ js-1/app.js
@@ -0,0 +1,37 @@
+// for (let i = 1; i <= 10; i++) {
+//     const element = i;
+//     console.log(element);
+// }
+
+//  for (let i = 1; i <= 10; i++) {
+//     console.log(`outer loop ${i}`);
+//     for (let j = 1; j <= 10; j++) {
+//         console.log(`inner loop ${j} outer loop ${i}`);
+//     }
+//  }
+
+// let myfam = ['Mosaref','bilis','taskin']
+
+// for (let index = 0; index < myfam.length; index++) {
+//     const element = myfam[index];
+//     console.log(element);
+// }
+
+
+// namta table print
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`namta table ${i}`);
+//     for (let j = 1; j <= 10; j++) {
+//         console.log(`${i} * ${j} = ${i*j}`);
+//     }
+// }
+
+// for (let i = 1; i <= 30; i++) {
+//     if (i == 5) {
+//         continue
+//     }
+//     // continue
+//     console.log(`this loop value is : ${i}`);
+// }
+
---
+++ js-1/doWhile.js
@@ -0,0 +1,18 @@
+// let i = 1;
+
+// while (i <= 10) {
+//     console.log(`${i} * 20 = ${i*20}`)
+//     i = i + 1
+// }
+
+// let num1 = 1
+// do {
+//     console.log(num1);
+//     num1++
+// } while (num1<= 10);
+
+let num2 = 15
+do {
+    console.log(num2);
+    num2++
+} while (num2<= 10);
