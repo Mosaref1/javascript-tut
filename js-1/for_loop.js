c5595495160bcd0d16a15eb55a739da799bb4a04
Mosaref1 <mosarefpaik763@gmail.com>
2023-11-22 17:30:27 +0530

    loop and map file and some change

--- js-1/for_loop.js
+++ js-1/for_loop.js
@@ -19,6 +19,60 @@ for (const name of str) {
     }
     // console.log(name);
 }
+//  for in loop 
 
+const myobj = {
+    name: 'Mosaref',
+    age: 22,
+    sub: "Secince",
+}
 
+for (const key in myobj) {
+    // console.log(`${key} : ${myobj[key]}`);
+}
+
+// result 
+// name : Mosaref
+// age : 22
+// sub : Secince
+
+// array.forEach(element => {
+    
+// });
+const myarry =['Mosaref','Mokadesh','Mofassel','Mariful']
+
+myarry.forEach( (val) => {
+    // console.log(val);
+} )
+
+let listarry = [
+    {
+        Name:"Mosaref",
+        Age: 22 ,
+        vill: "Uttarabad"
+    },
+    {
+        Name:"Bilkis",
+        Age: 20 ,
+        vill: "Jamalpur"
+    },
+    {
+        Name:"Mofassel",
+        Age: 25 ,
+        vill: "Kolkata"
+    },
+    {
+        Name:"Mokadesh",
+        Age: 32 ,
+        vill: "Santoshpur"
+    },
+]
+
+listarry.forEach( (val) => {
+    // console.log(val.Name);
+} )
+
+listarry.forEach( (val) => {
+    console.log(val.vill);
+} )
 
---
+++ js-1/map.js
@@ -0,0 +1,14 @@
+const mymap = new Map()
+
+mymap.set("WB" , 'West bengal')
+mymap.set("MI" , 'Mumbai')
+mymap.set("DL" , 'Delh')
+mymap.set("Bg" , 'Bangalore')
+
+for (const [key,value] of mymap) {
+    // console.log(key, ':-' , value);
+}
+
+
+
+
