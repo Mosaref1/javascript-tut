fcd90b9cbd18fa878342b41c85a191ceddcd8a12
Mosaref1 <mosarefpaik763@gmail.com>
2023-11-22 16:05:28 +0530

    add for off loop file

---
+++ js-1/for_loop.js
@@ -0,0 +1,24 @@
+const myfam = ['Mosaref','Bilis','Taskin']
+
+for (const name of myfam) {
+    // console.log(name);
+}
+
+const number = [1,2,3,4,5,6,7,8,9,10]
+
+for (const num of number) {
+    //console.log(num);
+}
+
+const str = "Mosaref paik"
+
+for (const name of str) {
+    if (name===" ") {
+        // console.log("Space detected");
+        continue
+    }
+    // console.log(name);
+}
+
+
+
