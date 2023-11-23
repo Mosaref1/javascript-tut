237423d5a74ae9d2e691e131669e51f1f17370df
Mosaref1 <mosarefpaik763@gmail.com>
2023-11-23 17:46:35 +0530

    add new dom and add body

--- js-2/dom2/dom2.js
+++ js-2/dom2/dom2.js
@@ -6,4 +6,19 @@ changeli.forEach( function (li) {
     li.style.color='red';
     li.style.dispaly='inline'
 })
-   
+
+
+const creatediv = document.createElement('div');
+
+creatediv.className='new_div';
+creatediv.id='mainId'
+creatediv.style.backgroundColor='green'
+creatediv.innerText='Mosaref paik'
+creatediv.style.padding='20px'
+creatediv.style.height='300px'
+creatediv.style.textAlign='center'
+
+
+
+document.body.appendChild(creatediv)
+console.log(creatediv);
