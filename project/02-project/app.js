3494faf1804c720903f55a928ffd212d762f03a7
Mosaref1 <mosarefpaik763@gmail.com>
2023-11-25 12:56:20 +0530

    add new commit

--- project/02-project/app.js
+++ project/02-project/app.js
@@ -1,17 +1,21 @@
-const form = document.querySelector('form')
+const form = document.querySelector("form")
 
-form.addEventListener('submit',function (e) {
+
+
+form.addEventListener('submit', function (e){
     e.preventDefault;
 
-    const height = parseInt(document.querySelector('#height'))
-    const wight = parseInt(document.querySelector('#weight'))
-    const result = document.querySelector("#Result")
+    let height =parseInt(document.querySelector("#height").value);
+    let weight = parseInt(document.querySelector("#weight").value);
+    let result = document.querySelector("#Result");
 
-
-    if (height === "" || height < 0 || isNaN(height)){
-        result.innerHTML=`plase give vile number ${height}`
+    if (height==='' || height < 0 || isNaN(height)) {
+        result.innerHTML='pls give vild number hight'
+    } else if (weight==='' || weight < 0 || isNaN(weight)) {
+        result.innerHTML=`pls vild weight ${weight}`
+    } else {
+        
     }
-    // result.innerHTML=`$(height)`
 })
- 
+
 
