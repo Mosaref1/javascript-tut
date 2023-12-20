22ec785ce0bbd3ee8141fcdeaf6a63f73a6988d3
Mosaref1 <mosarefpaik763@gmail.com>
2023-12-20 17:40:02 +0530

    basic js code

--- 4-js/script.js
+++ 4-js/script.js
@@ -1,5 +1,42 @@
 function fullName(fName,Lname){
-    console.log(fName,Lname);
+    // console.log(fName,Lname);
 }
 
 fullName("Mosaref","Paik")
+let maxVal = Number.MAX_SAFE_INTEGER;
+// console.log(maxVal);
+let minVal = Number.MIN_SAFE_INTEGER;
+// console.log(minVal);
+
+// let integer = Number.isInteger(12.6)
+let integer = Number.isSafeInteger(32)
+// console.log(integer);
+
+let bankbal = 7.787
+let newbal = 87.907
+// console.log(bankbal);
+newbal.toFixed()
+bankbal.toFixed()
+// console.log(bankbal + newbal);
+
+let num1 = Number.parseInt("23 year")
+
+// console.log(num1);
+
+let maxV = Number.MAX_VALUE;
+
+// console.log(maxV);
+let x = Number.EPSILON
+console.log(x);
+
+
+
+
+
+
+
+
+
+
+
+
